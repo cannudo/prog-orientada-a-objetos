eea341f7b3ddcca287723ad874f92c2f290b250c
Luan C. Redmann <luandacostaredmann@gmail.com>
2019-05-27 20:25:54 -0300

    Adding a simple registration for library users

--- SistemaBibliotecario/src/view/Main.java
+++ SistemaBibliotecario/src/view/Main.java
@@ -2,6 +2,7 @@ package view;
 import java.util.Scanner;
 
 import controller.Biblioteca;
+import model.Usuario;
 
 public class Main {
 	public static void main(String[] args) {
@@ -19,14 +20,33 @@ public class Main {
 			System.out.println("\nO que deseja fazer a seguir?");
 			System.out.println("0. Sair");
 			System.out.println("1. Ver dados da biblioteca");
+			System.out.println("2. Adicionar um usuário");
 			System.out.print("Digite um inteiro correspondente a opção --> ");
 			entrada = leitor.nextInt();
 			leitor.nextLine();
 			switch (entrada) {
+			case 0:
+				break;
 			case 1:
+				System.out.println("Você optou por ver dados da biblioteca");
 				System.out.println("\n" + biblioteca.toString());
 				break;
+			case 2:
+				System.out.println("Você optou por adicionar um usuário\n");
+				Usuario usuario = new Usuario();
+				System.out.print("Digite a matrícula do usuário: ");
+				usuario.setMatricula(leitor.nextLine());
+				System.out.print("Digite o nome do usuário: ");
+				usuario.setNome(leitor.nextLine());
+				System.out.print("Digite o curso do usuário: ");
+				usuario.setCurso(leitor.nextLine());
+				System.out.println("Usuário cadastrado com sucesso!");
+				break;
+			default:
+				System.out.println("\nOpção inválida.");
+				break;
 			}
+			
 		}
 		System.out.println("Sessão encerrada.");
 	}
