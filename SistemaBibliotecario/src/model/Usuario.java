11d9d52f0c4d418973187c52712e7d882e8b713d
Luan C. Redmann <luandacostaredmann@gmail.com>
2019-05-27 20:33:43 -0300

    Adding more fields in the user record for information consistency.

--- SistemaBibliotecario/src/model/Usuario.java
+++ SistemaBibliotecario/src/model/Usuario.java
@@ -1,8 +1,51 @@
 package model;
 
 public class Usuario {
-	private String nome;
 	private String matricula;
+	private String nome;
+	private String telefone;
+	private String email;
+	
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
 	private String curso;
 	
 	public Usuario() {
@@ -15,28 +58,6 @@ public class Usuario {
 		this.curso = curso;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getMatricula() {
-		return matricula;
-	}
-
-	public void setMatricula(String matricula) {
-		this.matricula = matricula;
-	}
-
-	public String getCurso() {
-		return curso;
-	}
-
-	public void setCurso(String curso) {
-		this.curso = curso;
-	}
+	
 	
 }
